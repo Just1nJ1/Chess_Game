491dc22dabfde25fbdc8fb9acfc1d0a2c1d77a5b
Justin <462499571@qq.com>
2018-02-27 19:18:28 +0800

    Initialize Chess Game.

---
+++ src/PrintName.java
@@ -0,0 +1,12 @@
+public class PrintName {
+    PrintName (){
+        System.out.println("Vincent");
+        System.out.println("Jeffery");
+        System.out.println("Justin");
+        System.out.println("John");
+    }
+
+    public static void main (String... args){
+        new PrintName ();
+    }
+}
