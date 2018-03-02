81187564ac89000270a8157c2571f3d0e5aa018d
Justin <462499571@qq.com>
2018-03-02 09:11:23 +0800

    add boolean property isMoved.

--- src/Main/Chessman.java
+++ src/Main/Chessman.java
@@ -4,6 +4,7 @@ public abstract class Chessman {
     public abstract boolean move ();
     public boolean isWhite;
     public char name;
+    public boolean isMoved = false;
 
     @Override
     public String toString() {
