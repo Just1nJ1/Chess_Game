1df4b986dc64ed449f13167a8414ebd804a28751
JefferyAS <jeffery2001@163.com>
2018-03-05 10:47:06 +0800

    Board

--- src/Main/Board.java
+++ src/Main/Board.java
@@ -1,11 +1,37 @@
 package Main;
 
+import Chessmans.*;
+
 public class Board {
     Chessman[][] board;
 
-    public Board() {
+    private Board() {
         board = new Chessman[8][8];
     }
+
+    public Board factor(){
+        Board board=new Board();
+        boolean isWhite=false;
+        for(int hor=1,i=0;i<2;i++){
+            for (int ver=0;ver<8;ver++){
+                board.board[ver][hor]=new Pawn(isWhite);
+            }
+            if(isWhite){hor+=1;}
+            else{hor-=1;}
+            for (int ver=0;ver<8;ver++){
+                board.board[ver][hor]=createChess(isWhite,ver);
+            }
+            isWhite=true;
+            hor=6;
+        }
+        return board;
+    }
+
+    private Chessman createChess(boolean isWhite, int number){
+        Chessman[] chessmen={new Rook(isWhite),new Knight(isWhite),new Bishop(isWhite),new Queen(isWhite),new King(isWhite)};
+        return chessmen[number];
+    }
+
 
     @Override
     public String toString() {
