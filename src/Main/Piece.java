086bd74a13af25ac3392096a7fa1803a2a39f868
Justin <462499571@qq.com>
2018-03-06 16:16:22 +0800

    change move method and add checkPath method in piece class.

--- src/Main/Piece.java
+++ src/Main/Piece.java
@@ -1,11 +1,28 @@
 package Main;
 
 public abstract class Piece {
-    public abstract boolean move (Piece[][] board, int startX,int startY,int goalX,int goalY);
     public boolean isWhite;
     public char name;
     public boolean isMoved = false;
     public Cell currentCell;
+
+    public boolean move (Piece board[][], int startX, int startY, int goalX, int goalY) {
+        if ( checkPath ( board, startX, startY, goalX, goalY) ){
+            board[goalY][goalX] = board[startY][startX];
+            board[startY][startX] = null;
+            return true;
+        }
+
+        return false;
+    }
+
+    public boolean checkPath (Piece[][] board, int startX, int startY, int goalX, int goalY){
+        if (board[goalY][goalX] == null || board[goalY][goalX].isWhite != isWhite){
+            return true;
+        }
+
+        return false;
+    }
 
     @Override
     public String toString() {
