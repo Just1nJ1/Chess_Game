d6435de2665551cce61b980327a59f20638d9171
26303 <2630334315@qq.com>
2018-03-13 15:01:22 +0800

    king

--- src/Piece/King.java
+++ src/Piece/King.java
@@ -10,7 +10,37 @@ public class King extends Piece {
     }
 
     @Override
-    public boolean move(Piece[][] board, int startX, int startY, int goalX, int goalY) {
+    public boolean checkPath(Piece[][] board, int startX, int startY, int goalX, int goalY) {
+        boolean move = true;
+        if(goalX == startX+1 || goalX == startX-1 || goalX == startX)
+        {
+            if(goalY == startY+1 || goalY == startY-1 || goalY == startY)
+            {
+                if(startX == goalX) {
+                    if(!(super.checkPath(board,startX,startY,goalX,goalY))){
+                        move = false;
+                    }
+                    if(move = true) {
+                        super.move(board, startX, startY, goalX, goalY);
+                    }
+                }
+
+                else if(startY == goalY) {
+                    if(!(super.checkPath(board,startX,startY,goalX,goalY))) {
+                        move = false;
+                    }
+                    if(move = true) {
+                        super.move(board, startX, startY, goalX, goalY);
+                    }
+                }
+
+                else if(!(super.checkPath(board, startX, startY, goalX, goalY)))
+                    if(move = true)
+                    {
+                        super.move(board, startX, startY, goalX, goalY);
+                    }
+            }
+        }
         return false;
     }
 }
