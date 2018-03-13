dba0d67dee2bb629a753015b37d36d9d0170813d
LTY66666 <m13011219023@163.com>
2018-03-13 15:01:35 +0800

    bishop

--- src/Piece/Bishop.java
+++ src/Piece/Bishop.java
@@ -10,7 +10,34 @@ public class Bishop extends Piece {
     }
 
     @Override
-    public boolean move(Piece[][] board, int startX, int startY, int goalX, int goalY) {
+    public boolean checkPath(Piece[][] board, int startX, int startY, int goalX, int goalY) {
+        if(Math.abs(startX-goalX)==Math.abs(startY-goalY)){
+            int dirX = 0;
+            int dirY = 0;
+            if(goalX-startX>0){
+                dirX=1;
+            }
+            if(goalX-startX<0){
+                dirX=-1;
+            }
+            if(goalY-startY>0){
+                dirY=1;
+            }
+            if(goalY-startY<0){
+                dirY=-1;
+            }
+            if(goalX-startX==goalY-startY){
+                for(int i=0;i<=goalX;i++){
+                    i=i*dirX*dirY;
+                    if(!(super.checkPath(board, startX, startY, startX+i, startY+i))){
+                        return false;
+                    }
+                    if(i==goalX){
+                        return true;
+                    }
+                }
+            }
+        }
         return false;
     }
 }
