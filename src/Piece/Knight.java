9e7b63872805dcb82279c1a06064df3d9777970c
Justin <462499571@qq.com>
2018-03-14 21:03:47 +0800

    override checkPath method.

--- src/Piece/Knight.java
+++ src/Piece/Knight.java
@@ -11,4 +11,12 @@ public class Knight extends Piece {
     public static Piece factor(boolean isWhite){
         return new Knight(isWhite);
     }
+
+    @Override
+    public boolean checkPath (Piece[][] board, int startX, int startY, int goalX, int goalY){
+        int dX = Math.abs ( goalX - startX );
+        int dY = Math.abs ( goalY - startY );
+        if (dX + dY != 3 || dX * dY != 2){ return false; }
+        else { return super.checkPath ( board, startX, startY, goalX, goalY ); }
+    }
 }
