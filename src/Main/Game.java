9fd390585c9bf02d7044887811fbd9c7a8091169
JefferyAS <jeffery2001@163.com>
2018-03-15 08:40:45 +0800

    Board

--- src/Main/Game.java
+++ src/Main/Game.java
@@ -2,10 +2,10 @@ package Main;
 import java.util.Scanner;
 public class Game {
     Board board;
-
+    boolean turn;
     public Game (){
         board=Board.factor();
-
+        turn=true;
         //initialChess ();
     }
 
@@ -13,27 +13,25 @@ public class Game {
      *
      * @param turn
      */
-    private void move(boolean turn){
+    public void move(String startP,String startE,boolean turn){
         System.out.print("type");
-        int[] start=getLocate();
-        int[] end=getLocate();
+        int[] start=getLocate(startP);
+        int[] end=getLocate(startE);
         Piece chess=board.getBoard()[start[1]][start[0]];
         if (chess==null){
             System.out.print("no chess");
-            move(turn);
-            return;
+            throw new Error("wrong input");
         }
         if (!chess.getIsWhite()==turn){
             System.out.print("wrong chess");
-            move(turn);
-            return;
+            throw new Error("wrong input");
         }
         if(chess.checkPath(board.getBoard(),start[0],start[1],end[0],end[1])){
+            turn=!turn;
             chess.move(board.getBoard(),start[0],start[1],end[0],end[1]);
         }
         else {
-            System.out.print("illegal path");
-            move(turn);
+            throw new Error("wrong input");
         }
     }
 
@@ -41,18 +39,16 @@ public class Game {
      *
      * @return
      */
-    private int[] getLocate(){
-        Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
+
+
+    private int[] getLocate(String input){
         if (input.length()!=2){
-            System.out.print("wrong input");
-            return getLocate();
+            throw new Error("wrong input");
         }
         int ver=8-Integer.valueOf((input.substring(0,1)));
         int hor=input.charAt(1)-97;
         if (hor>8||hor<0||ver>8||ver<0){
-            System.out.print("wrong input");
-            return getLocate();
+            throw new Error("wrong input");
         }
         int[] output={hor,ver};
         return output;
