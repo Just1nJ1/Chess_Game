98c1493cc88ed4670d4aa62ca123548d6928abbf
26303 <2630334315@qq.com>
2018-03-16 13:00:35 +0800

    king

--- src/test.java
+++ src/test.java
@@ -5,7 +5,7 @@ public class test {
     public static void main(String... args){
         Game test1 = new Game();
         System.out.print(test1);
-        test1.move(true);
+        test1.move(String startP,String startE));
         System.out.print(test1);
     }
 }
