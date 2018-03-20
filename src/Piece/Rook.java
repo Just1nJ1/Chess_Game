4320ea5b39f0b7fe47d574b3d81245035cbbcdfd
wang <hahha__@outlook.com>
2018-03-20 15:07:39 +0800

    test

--- src/Piece/Rook.java
+++ src/Piece/Rook.java
@@ -18,7 +18,7 @@ public class Rook extends Piece {
         boolean flag = true;
         if(startX == goalX){
             for(int i=0; i <= Math.abs(goalX-startX); i++){
-                if(!(super.checkPath(board,startX,startY,goalX,startY + ((goalX-startX)/Math.abs(goalX-startX))*i))){
+                if(!(super.checkPath(board,startX,startY,goalX,startY + ((goalY-startY)/Math.abs(goalY-startY))*i))){
                     return false;
                 }
             }
@@ -27,23 +27,13 @@ public class Rook extends Piece {
         }
         else if(startY == goalY){
             for(int i=0; i<=Math.abs(goalY-startY); i++){
-                if(!(super.checkPath(board,startX,startY,startX + ((goalX-startX)/Math.abs(goalX-startX))*i,startY))){
+                if(!(super.checkPath(board,startX,startY,startX + ((goalX-startX)/Math.abs(goalX-startX))*i,goalY))){
                     return false;
                 }
             }
             //super.move(board,startX,startY,goalX,goalY);
             return true;
         }
-//        if(startX == goalX | startY == goalY){
-//            int x = goalX - startX;
-//            int y = goalY - startY;
-//            if(x != 0){
-//                int tempStartX = startX;
-//                for(int i=0; i != Math.abs(x); i++){
-//                    checkPath(board,startX,startY,startX,startY);
-//                }
-//            }
-//        }
         return false;
     }
 }
